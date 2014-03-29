606c9731bb571a99abcbaf288fbab7b3606eff66
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-28 23:49:06 -0300

    applying comments in class EFalhaNaturezaController.php

--- exceptions/EFalhaNaturezaController.php
+++ exceptions/EFalhaNaturezaController.php
@@ -2,9 +2,11 @@
 /*
  File name: EFalhaNaturezaController.php
  File description: exception for kind functions
- Authors: Lucas Andrade, Eduardo Augusto, Sérgio Bezerra, Lucas Carvalho, Eliseu
+ Authors: Lucas Andrade, Eduardo Augusto, Sï¿½rgio Bezerra, Lucas Carvalho, Eliseu
 */
+
 class EFalhaNaturezaController extends Exception{
+//customizing exception to throw a message when exception is triggered
 
 	public function __construct(){
 		$this->message = "Erro em alguma funcao na classe Natureza";
