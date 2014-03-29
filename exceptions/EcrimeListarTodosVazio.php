ddf860d92ebc8ef6b093e0d26ff9c2121950adbb
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-28 23:36:30 -0300

    applying comments in class EcrimeListarTodosVazio.php

--- exceptions/EcrimeListarTodosVazio.php
+++ exceptions/EcrimeListarTodosVazio.php
@@ -2,9 +2,11 @@
 /*
  File name: EcrimeListarTodosVazio.php
  File description: exception for list crime
- Authors: Lucas Andrade, Eduardo Augusto, Sérgio Bezerra, Lucas Carvalho, Eliseu
+ Authors: Lucas Andrade, Eduardo Augusto, Sï¿½rgio Bezerra, Lucas Carvalho, Eliseu
 */
+
 class EcrimeListarTodosVazio extends Exception{
+//customizing exception to throw a message when exception is triggered
 
 	public function __construct(){
 		$this->message = "Problema ao listar crimes.";
