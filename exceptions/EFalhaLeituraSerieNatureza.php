f9267357003930522d0f610327d755b32ae438fa
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-28 23:54:33 -0300

    applying comments in class EFalhaLeituraSerieCrime.php and EFalhaLeituraSerieNatureza.php

--- exceptions/EFalhaLeituraSerieNatureza.php
+++ exceptions/EFalhaLeituraSerieNatureza.php
@@ -2,9 +2,11 @@
 /*
  File name: EFalhaLeituraSerieNatureza.php
  File description: exception for read kind
- Authors: Lucas Andrade, Eduardo Augusto, Sérgio Bezerra, Lucas Carvalho, Eliseu
+ Authors: Lucas Andrade, Eduardo Augusto, Sï¿½rgio Bezerra, Lucas Carvalho, Eliseu
 */
+
 class EFalhaLeituraSerieNatureza extends Exception{
+//customizing exception to throw a message when exception is triggered
 	
 	public function __construct(){
 		$this->message = "Falha na leitura de naturezas da planilha serie historica!";
