ae0ec8aa2c9b4f1554b317ff6d5eddd535046312
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-29 00:03:31 -0300

    applying comments in class ECrimeConsultarIdNaturezaVazio.php

--- exceptions/ECrimeConsultarIdNaturezaVazio.php
+++ exceptions/ECrimeConsultarIdNaturezaVazio.php
@@ -2,10 +2,12 @@
 /*
  File name: ECrimeConsultarIdNaturezaVazio.php
  File description: exception for consult crime id
- Authors: Lucas Andrade, Eduardo Augusto, Sérgio Bezerra, Lucas Carvalho, Eliseu
+ Authors: Lucas Andrade, Eduardo Augusto, Sï¿½rgio Bezerra, Lucas Carvalho, Eliseu
 */
-class ECrimeConsultarIdNaturezaVazio extends Exception{
 
+class ECrimeConsultarIdNaturezaVazio extends Exception{
+//customizing exception to throw a message when exception is triggered
+    
 	public function __construct(){
 		$this->message = "Erro ao consultar o ID da natureza.";
 	}
