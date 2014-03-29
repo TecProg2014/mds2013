bdb7c4433704bc6485a04eea3606e8f3c2838657
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-28 23:56:38 -0300

    applying comments in class EFalhaLeituraSerieCategoria.php

--- exceptions/EFalhaLeituraSerieCategoria.php
+++ exceptions/EFalhaLeituraSerieCategoria.php
@@ -2,9 +2,11 @@
 /*
  File name: EFalhaLeituraSerieCategoria.php
  File description: exception for read kind
- Authors: Lucas Andrade, Eduardo Augusto, Sérgio Bezerra, Lucas Carvalho, Eliseu
+ Authors: Lucas Andrade, Eduardo Augusto, Sï¿½rgio Bezerra, Lucas Carvalho, Eliseu
 */
+
 class EFalhaLeituraSerieCategoria extends Exception{
+//customizing exception to throw a message when exception is triggered
 	
 	public function __construct(){
 		$this->message = "Falha na leitura de categoria da planilha de serie historica!";
