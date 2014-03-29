66c5b0ab07fbb1a1b9beeb52e795d7f223aa2d83
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-29 00:06:41 -0300

    applying comments in clas sECategoriaListarTodasAfabelicamenteVazio.php

--- exceptions/ECategoriaListarTodasAlfabeticamenteVazio.php
+++ exceptions/ECategoriaListarTodasAlfabeticamenteVazio.php
@@ -2,9 +2,11 @@
 /*
  File name: ECategoriaListarTodasAfabelicamenteVazio.php
  File description: exception for list categories alphabetical
- Authors: Lucas Andrade, Eduardo Augusto, Sérgio Bezerra, Lucas Carvalho, Eliseu
+ Authors: Lucas Andrade, Eduardo Augusto, Sï¿½rgio Bezerra, Lucas Carvalho, Eliseu
 */
+
 class ECategoriaListarTodasAlfabeticamenteVazio extends Exception{
+//customizing exception to throw a message when exception is triggered
 
 	public function __construct(){
 		$this->message = "Problema ao listar categorias alfabeticamente.";
