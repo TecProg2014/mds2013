6e9f5b59fda302c1106feba27dd0747fc825dc42
suzukijessica <suzuki_jessica@hotmail.com>
2014-04-02 17:20:51 -0300

    Applying coments on model tempo

--- model/Tempo.php
+++ model/Tempo.php
@@ -12,36 +12,40 @@ class Tempo{
 	
 	
 	public function __setIdTempo($idTempo){
-		
+	//Method to modify the instance of the time attribute 	
 		if(!is_numeric($idTempo)){
 			throw new ETipoErrado();
 		}
 		$this->idTempo = $idTempo;
 	}
 	public function __getIdTempo(){
+        //Method to access the instance of time attribute
 		return $this->idTempo;
 	}
 	public function __setIntervalo($intervalo){
-		
+	//Method to modify the instance of the interval attribute 	
 		if(!is_numeric($intervalo)){
 			throw new ETipoErrado();
 		}
 		$this->intervalo = $intervalo;
 	}
 	public function __getIntervalo(){
+        //Method to access the instance of interval attribute
 		return $this->intervalo;
 	}
 	public function __setMes($mes){
+        //Method to modify the instance of the month attribute 
 		$this->mes = $mes;
 	}
 	public function __getMes(){
+        //Method to access the instance of month attribute
 		return  $this->mes;
 	}
 	public function __construct(){
-		
+	//Default constructor method of class	
 	}
 	public function __constructOverload($idTempo,$intervalo,$mes){
-		
+	//Constructor method of class	
 		$this->idTempo = $idTempo;
 		$this->intervalo = $intervalo;
 		$this->mes = $mes;
