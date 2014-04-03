9f354185963d6b8f310e2d58021e9278600ae03d
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-04-02 21:28:34 -0300

    applying commentsr iin class "RegiaoAdministrativaController.php"

--- controller/RegiaoAdministrativaController.php
+++ controller/RegiaoAdministrativaController.php
@@ -2,8 +2,9 @@
 /*
  File name: RegiaoAdministrativaController.php
  File description: insert, consult, show and sum some administrative region informations
- Authors: Lucas Andrade, Eduardo, Sérgio, Lucas, Eliseu
+ Authors: Lucas Andrade, Eduardo, Sï¿½rgio, Lucas, Eliseu
 */
+
 include_once('C:/xampp/htdocs/mds2013/persistence/RegiaoAdministrativaDAO.php');
 include_once('C:/xampp/htdocs/mds2013/exceptions/EErroConsulta.php');
 include_once('C:/xampp/htdocs/mds2013/model/RegiaoAdministrativa.php');
@@ -12,24 +13,34 @@ class RegiaoAdministrativaController {
 	private $raDAO;
 	
 	public function __construct(){
-		$this->raDAO = new RegiaoAdministrativaDAO();
+                $this->raDAO = new RegiaoAdministrativaDAO();
 	}
 	
 	public function _listarTodas(){
+        //lists administrative regions
 		$arrayRA = $this->raDAO->listarTodas();
 		return $arrayRA;
 	}
+        
 	public function __constructTeste(){
+        //tests instance raDAO
 		$this->raDAO->__constructTeste();
 	}
+        
 	public function _listarTodasAlfabeticamente(){
+        //lists administrative regions alphabetically
+            
 		$arrayRA = $this->raDAO->listarTodasAlfabeticamente();
+                
+                //loop for order administrative regions alphabetically
 		for($i=0;$i<(count($arrayRA));$i++){
 			$nomeRA[] = $arrayRA[$i]->__getNomeRegiao();
 		}
 		return $nomeRA;
 	}
+        
 	public function _consultarPorId($id){
+        //consults administrative region by its id
 		
 		if(!is_numeric($id)){
 			throw new EErroConsulta();
@@ -38,7 +49,9 @@ class RegiaoAdministrativaController {
 		$RA =  $this->raDAO->consultarPorId($id);
 		return $RA;
 	}
+        
 	public function _consultarPorNome($nome){
+        //consults administrative regions by their names
 	
 		if(!is_string($nome)){
 			throw new EErroConsulta();
@@ -48,9 +61,12 @@ class RegiaoAdministrativaController {
 	}
 	
 	public function _contarRegistrosRA(){
+        //counts administrative regions' records
 		return $this->raDAO->contarRegistrosRA();
 	}
+        
 	public function _inserirRA(RegiaoAdministrativa $RA){
+        //inserts administrative regions
 		return $this->raDAO->inserirRA($RA);
 	}
 	
