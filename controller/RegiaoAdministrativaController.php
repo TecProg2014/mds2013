69db3b6172a4ddea4c16b417c97e3af5754eb536
Tainara Reis <tainara.s.r@hotmail.com>
2014-04-20 12:51:58 -0300

    Refactoring variables from the class RegiaoAdministrativaController.php

--- controller/RegiaoAdministrativaController.php
+++ controller/RegiaoAdministrativaController.php
@@ -11,27 +11,27 @@ include_once('C:/xampp/htdocs/mds2013/model/RegiaoAdministrativa.php');
 
 class RegiaoAdministrativaController {
 
-    private $raDAO;
+    private $administrativeRegionDAO;
 
     public function __construct() {
-        $this->raDAO = new RegiaoAdministrativaDAO();
+        $this->administrativeRegionDAO = new RegiaoAdministrativaDAO();
     }
 
     public function _listarTodas() {
         //lists administrative regions
-        $arrayRA = $this->raDAO->listarTodas();
-        return $arrayRA;
+        $arrayOfAdministrativeRegions = $this->administrativeRegionDAO->listarTodas();
+        return $arrayOfAdministrativeRegions;
     }
 
     public function __constructTeste() {
         //tests instance raDAO
-        $this->raDAO->__constructTeste();
+        $this->administrativeRegionDAO->__constructTeste();
     }
 
     public function _listarTodasAlfabeticamente() {
         //lists administrative regions alphabetically
 
-        $arrayRA = $this->raDAO->listarTodasAlfabeticamente();
+        $arrayRA = $this->administrativeRegionDAO->listarTodasAlfabeticamente();
 
         //loop for order administrative regions alphabetically
         for ($i = 0; $i < (count($arrayRA)); $i++) {
@@ -47,7 +47,7 @@ class RegiaoAdministrativaController {
             throw new EErroConsulta();
         }
 
-        $RA = $this->raDAO->consultarPorId($id);
+        $RA = $this->administrativeRegionDAO->consultarPorId($id);
         return $RA;
     }
 
@@ -57,25 +57,25 @@ class RegiaoAdministrativaController {
         if (!is_string($nome)) {
             throw new EErroConsulta();
         }
-        $RA = $this->raDAO->consultarPorNome($nome);
+        $RA = $this->administrativeRegionDAO->consultarPorNome($nome);
         return $RA;
     }
 
     public function _contarRegistrosRA() {
         //counts administrative regions' records
-        return $this->raDAO->contarRegistrosRA();
+        return $this->administrativeRegionDAO->contarRegistrosRA();
     }
 
     public function _inserirRA(RegiaoAdministrativa $RA) {
         //inserts administrative regions
-        return $this->raDAO->inserirRA($RA);
+        return $this->administrativeRegionDAO->inserirRA($RA);
     }
 
     public function _inserirRegiaoArrayParseRA($arrayRA) {
         for ($i = 0; $i < count($arrayRA); $i++) {
             $dadosRegiao = new RegiaoAdministrativa();
             $dadosRegiao->__setNomeRegiao($arrayRA[$i]);
-            $this->raDAO->inserirRA($dadosRegiao);
+            $this->administrativeRegionDAO->inserirRA($dadosRegiao);
         }
     }
 
