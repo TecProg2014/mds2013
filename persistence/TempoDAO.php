089d23e7c8d72d09245dc8f82d31eb646e4c5ee1
suzukijessica <suzuki_jessica@hotmail.com>
2014-04-22 22:23:31 -0300

    Changing variables languages (TempoDAO.php)

--- persistence/TempoDAO.php
+++ persistence/TempoDAO.php
@@ -10,59 +10,59 @@ include_once('C:/xampp/htdocs/mds2013/persistence/ConexaoTeste.php');
 
 class TempoDAO {
 
-    private $conexao;
+    private $connection;
 
     public function __construct() {
-        $this->conexao = new Conexao();
+        $this->connection = new Conexao();
     }
 
     public function __constructTeste() {
-        $this->conexao = new ConexaoTeste();
+        $this->connection = new ConexaoTeste();
     }
 
     public function listarTodos() {
         $sql = "SELECT * FROM tempo";
-        $resultado = $this->conexao->banco->Execute($sql);
-        while ($registro = $resultado->FetchNextObject()) {
-            $dadosTempo = new Tempo();
-            $dadosTempo->__constructOverload($registro->ID_TEMPO, $registro->ANO, $registro->MES);
-            $retornaTempos[] = $dadosTempo;
+        $result = $this->connection->banco->Execute($sql);
+        while ($registry = $result->FetchNextObject()) {
+            $dataTime = new Tempo();
+            $dataTime->__constructOverload($registry->ID_TEMPO, $registry->ANO, $registry->MES);
+            $resultOfTime[] = $dataTime;
         }
-        return $retornaTempos;
+        return $resultOfTime;
     }
 
     public function listarTodasEmOrdem() {
         $sql = "SELECT * FROM tempo ORDER BY ano ASC ";
-        $resultado = $this->conexao->banco->Execute($sql);
-        while ($registro = $resultado->FetchNextObject()) {
-            $dadosTempo = new Tempo();
-            $dadosTempo->__constructOverload($registro->ID_TEMPO, $registro->ANO, $registro->MES);
-            $retornaTempos[] = $dadosTempo;
+        $result = $this->connection->banco->Execute($sql);
+        while ($registry = $result->FetchNextObject()) {
+            $dataTime = new Tempo();
+            $dataTime->__constructOverload($registry->ID_TEMPO, $registry->ANO, $registry->MES);
+            $resultOfTime[] = $dataTime;
         }
-        return $retornaTempos;
+        return $resultOfTime;
     }
 
     public function consultarPorId($id) {
         $sql = "SELECT * FROM tempo WHERE id_tempo = '" . $id . "'";
-        $resultado = $this->conexao->banco->Execute($sql);
-        $registro = $resultado->FetchNextObject();
-        $dadosTempo = new Tempo();
-        $dadosTempo->__constructOverload($registro->ID_TEMPO, $registro->ANO, $registro->MES);
-        return $dadosTempo;
+        $result = $this->connection->banco->Execute($sql);
+        $registry = $result->FetchNextObject();
+        $dataTime = new Tempo();
+        $dataTime->__constructOverload($registry->ID_TEMPO, $registry->ANO, $registry->MES);
+        return $dataTime;
     }
 
     public function consultarPorIntervalo($intervalo) {
         $sql = "SELECT * FROM tempo WHERE ano = '" . $intervalo . "'";
-        $resultado = $this->conexao->banco->Execute($sql);
-        $registro = $resultado->FetchNextObject();
-        $dadosTempo = new Tempo();
-        $dadosTempo->__constructOverload($registro->ID_TEMPO, $registro->ANO, $registro->MES);
-        return $dadosTempo;
+        $result = $this->connection->banco->Execute($sql);
+        $registry = $result->FetchNextObject();
+        $dataTime = new Tempo();
+        $dataTime->__constructOverload($registry->ID_TEMPO, $registry->ANO, $registry->MES);
+        return $dataTime;
     }
 
     public function inserirTempo(Tempo $tempo) {
         $sql = "INSERT INTO tempo (ano) VALUES ('{$tempo->__getIntervalo()}')";
-        $this->conexao->banco->Execute($sql);
+        $this->connection->banco->Execute($sql);
     }
 
 }
