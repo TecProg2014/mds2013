fbb1176cff15e2666c6e0591a3f50cc2e1753fd1
suzukijessica <suzuki_jessica@hotmail.com>
2014-04-22 21:39:49 -0300

    Changing variables languages (util)

--- util/RunParse.php
+++ util/RunParse.php
@@ -1,25 +1,25 @@
 <?php
 
 require_once ('C:/xampp/htdocs/mds2013/util/Parse.php');
-include_once ('C:/xampp/htdocs/mds2013/controller/CategoriaController.php');
-include_once ('C:/xampp/htdocs/mds2013/controller/CrimeController.php');
-include_once ('C:/xampp/htdocs/mds2013/controller/NaturezaController.php');
-include_once ('C:/xampp/htdocs/mds2013/controller/TempoController.php');
+include_once ('C:/xampp/htdocs/mds2013/controller/category_controllerntroller.php');
+include_once ('C:/xampp/htdocs/mds2013/controller/crime_controllerntroller.php');
+include_once ('C:/xampp/htdocs/mds2013/controller/nature_controlntroller.php');
+include_once ('C:/xampp/htdocs/mds2013/controller/time_controlntroller.php');
 
 class RunParse {
 
     private $parse;
-    private $categoriaCO;
-    private $crimeCO;
-    private $naturezaCO;
-    private $tempoCO;
+    private $category_controller;
+    private $crime_controller;
+    private $nature_control;
+    private $time_control;
 
     public function __construct() {
 
-        $this->categoriaCO = new CategoriaController();
-        $this->crimeCO = new CrimeController();
-        $this->naturezaCO = new NaturezaController();
-        $this->tempoCO = new TempoController();
+        $this->category_controller = new category_controllerntroller();
+        $this->crime_controller = new crime_controllerntroller();
+        $this->nature_control = new nature_controlntroller();
+        $this->time_control = new time_controlntroller();
 
 
         $this->parse = new Parse("JAN_SET_2011_12  POR REGIAO ADM_2.xls");
