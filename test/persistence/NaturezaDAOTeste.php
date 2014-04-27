6291a8f68c62a09f4428a54397006fe1d45490d3
Tainara Reis <tainara.s.r@hotmail.com>
2014-04-27 18:40:37 -0300

    Changing NaturezaDAOTeste

--- test/persistence/NaturezaDAOTeste.php
+++ test/persistence/NaturezaDAOTeste.php
@@ -14,35 +14,35 @@ class NaturezaDAOTeste extends PHPUnit_Framework_Testcase {
         $object_nature_DAO = new NaturezaDAO();
         $nature_dao_instance->assertObjectHasAttribute('conexao', $object_nature_DAO);
         $nature_dao_instance->assertInstanceOf('NaturezaDAO', $object_nature_DAO);
-        $nature_dao_instance->assertNotEmpty($object_nature_DAO->listarTodas());
-        $nature_dao_instance->assertNotNull($object_nature_DAO->listarTodas());
+        $nature_dao_instance->assertNotEmpty($object_nature_DAO->listAllAdministrativeRegion());
+        $nature_dao_instance->assertNotNull($object_nature_DAO->listAllAdministrativeRegion());
     }
 
     public function testListarTodasAlfabeticamente() {
         $object_nature_DAO = new NaturezaDAO();
         $nature_dao_instance->assertObjectHasAttribute('conexao', $object_nature_DAO);
         $nature_dao_instance->assertInstanceOf('NaturezaDAO', $object_nature_DAO);
-        $nature_dao_instance->assertNotEmpty($object_nature_DAO->listarTodas());
-        $nature_dao_instance->assertNotNull($object_nature_DAO->listarTodas());
+        $nature_dao_instance->assertNotEmpty($object_nature_DAO->listAllAdministrativeRegion());
+        $nature_dao_instance->assertNotNull($object_nature_DAO->listAllAdministrativeRegion());
     }
 
     public function testConsultarPorId() {
         $object_nature_DAO = new NaturezaDAO();
         $nature_dao_instance->assertObjectHasAttribute('conexao', $object_nature_DAO);
         $nature_dao_instance->assertInstanceOf('NaturezaDAO', $object_nature_DAO);
-        $nature_dao_instance->assertInstanceOf('Natureza', $object_nature_DAO->consultarPorId(1));
+        $nature_dao_instance->assertInstanceOf('Natureza', $object_nature_DAO->consultAdministrativeRegionById(1));
     }
 
     public function testConsultarPorNome() {
         $object_nature_DAO = new NaturezaDAO();
         $nature_dao_instance->assertObjectHasAttribute('conexao', $object_nature_DAO);
         $nature_dao_instance->assertInstanceOf('NaturezaDAO', $object_nature_DAO);
-        $nature_dao_instance->assertInstanceOf('Natureza', $object_nature_DAO->consultarPorNome('Estupro'));
+        $nature_dao_instance->assertInstanceOf('Natureza', $object_nature_DAO->consultAdministrativeRegionByName('Estupro'));
     }
 
     public function testInserirNatureza() {
         $object_nature_DAO = new NaturezaDAO();
-        $object_nature_DAO->__constructTeste();
+        $object_nature_DAO->__constructTest();
         $nature_dao_instance->assertObjectHasAttribute('conexao', $object_nature_DAO);
         $nature_dao_instance->assertInstanceOf('NaturezaDAO', $object_nature_DAO);
         $object_nature_DAO->inserirNatureza(new Natureza());
@@ -50,7 +50,7 @@ class NaturezaDAOTeste extends PHPUnit_Framework_Testcase {
 
     public function testConsultarPorIdCategoria() {
         $object_nature_DAO = new NaturezaDAO();
-        $object_nature_DAO->__constructTeste();
+        $object_nature_DAO->__constructTest();
         $nature_dao_instance->assertObjectHasAttribute('conexao', $object_nature_DAO);
         $nature_dao_instance->assertInstanceOf('NaturezaDAO', $object_nature_DAO);
         $nature_dao_instance->assertArrayHasKey(1, $object_nature_DAO->consultarPorIdCategoria(1));
