b44e163c3c28252a97be98e6ac69ed7ab6a084a6
Tainara Reis <tainara.s.r@hotmail.com>
2014-04-27 18:40:48 -0300

    Changing  RegiaoAdministrativaDAOTeste

--- test/persistence/RegiaoAdministrativaDAOTeste.php
+++ test/persistence/RegiaoAdministrativaDAOTeste.php
@@ -15,47 +15,47 @@ class RegiaoAdministrativaDAOTeste extends PHPUnit_Framework_TestCase {
         $object_region_adm_DAO = new RegiaoAdministrativaDAO();
         $region_adm_dao_instance->assertObjectHasAttribute('conexao', $object_region_adm_DAO);
         $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativaDAO', $object_region_adm_DAO);
-        $region_adm_dao_instance->assertNotEmpty($object_region_adm_DAO->listarTodas());
-        $region_adm_dao_instance->assertNotNull($object_region_adm_DAO->listarTodas());
+        $region_adm_dao_instance->assertNotEmpty($object_region_adm_DAO->listAllAdministrativeRegion());
+        $region_adm_dao_instance->assertNotNull($object_region_adm_DAO->listAllAdministrativeRegion());
     }
 
     public function testListarTodasAlfabeticamente() {
         $object_region_adm_DAO = new RegiaoAdministrativaDAO();
         $region_adm_dao_instance->assertObjectHasAttribute('conexao', $object_region_adm_DAO);
         $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativaDAO', $object_region_adm_DAO);
-        $region_adm_dao_instance->assertNotEmpty($object_region_adm_DAO->listarTodasAlfabeticamente());
-        $region_adm_dao_instance->assertNotNull($object_region_adm_DAO->listarTodasAlfabeticamente());
+        $region_adm_dao_instance->assertNotEmpty($object_region_adm_DAO->listAlphabeticallyAllAdministrativeRegions());
+        $region_adm_dao_instance->assertNotNull($object_region_adm_DAO->listAlphabeticallyAllAdministrativeRegions());
     }
 
     public function testConsultarPorId() {
         $object_region_adm_DAO = new RegiaoAdministrativaDAO();
         $region_adm_dao_instance->assertObjectHasAttribute('conexao', $object_region_adm_DAO);
         $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativaDAO', $object_region_adm_DAO);
-        $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativa', $object_region_adm_DAO->consultarPorId(1));
-        $region_adm_dao_instance->assertObjectHasAttribute('idRegiaoAdministrativa', $object_region_adm_DAO->consultarPorId(1));
+        $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativa', $object_region_adm_DAO->consultAdministrativeRegionById(1));
+        $region_adm_dao_instance->assertObjectHasAttribute('idRegiaoAdministrativa', $object_region_adm_DAO->consultAdministrativeRegionById(1));
     }
 
     public function testConsultarPorNome() {
         $object_region_adm_DAO = new RegiaoAdministrativaDAO();
         $region_adm_dao_instance->assertObjectHasAttribute('conexao', $object_region_adm_DAO);
         $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativaDAO', $object_region_adm_DAO);
-        $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativa', $object_region_adm_DAO->consultarPorNome('PLANALTINA'));
-        $region_adm_dao_instance->assertObjectHasAttribute('idRegiaoAdministrativa', $object_region_adm_DAO->consultarPorNome('PLANALTINA'));
+        $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativa', $object_region_adm_DAO->consultAdministrativeRegionByName('PLANALTINA'));
+        $region_adm_dao_instance->assertObjectHasAttribute('idRegiaoAdministrativa', $object_region_adm_DAO->consultAdministrativeRegionByName('PLANALTINA'));
     }
 
     public function testContarRegistrosRA() {
         $object_region_adm_DAO = new RegiaoAdministrativaDAO();
         $region_adm_dao_instance->assertObjectHasAttribute('conexao', $object_region_adm_DAO);
         $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativaDAO', $object_region_adm_DAO);
-        $region_adm_dao_instance->assertEquals(32, $object_region_adm_DAO->contarRegistrosRA());
+        $region_adm_dao_instance->assertEquals(32, $object_region_adm_DAO->_countAdministrativeRegionsRegisters());
     }
 
     public function testInserirRA() {
         $object_region_adm_DAO = new RegiaoAdministrativaDAO();
-        $object_region_adm_DAO->__constructTeste();
+        $object_region_adm_DAO->__constructTest();
         $region_adm_dao_instance->assertObjectHasAttribute('conexao', $object_region_adm_DAO);
         $region_adm_dao_instance->assertInstanceOf('RegiaoAdministrativaDAO', $object_region_adm_DAO);
-        $object_region_adm_DAO->inserirRA(new RegiaoAdministrativa());
+        $object_region_adm_DAO->insertAdministrativeRegion(new AdministrativeRegion());
     }
 
 }
