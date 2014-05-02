626f3851ba268f0020bcfe6e242c472701f53300
Yeltsinn <yeltsin.lobato@hotmail.com>
2014-05-02 17:24:30 -0300

    Changing Class TestConnection

--- persistence/TestConnection.php
+++ persistence/TestConnection.php
@@ -11,23 +11,23 @@ require_once('C:/xampp/htdocs/mds2013/exceptions/EConexaoFalha.php');
 class ConexaoTeste {
 
     public $banco;
-    private $tipo_banco;
-    private $servidor;
-    private $usuario;
-    private $senha;
+    private $dataBase_type;
+    private $server;
+    private $user;
+    private $password;
     private $db;
 
     public function __construct() {
 
-        $this->tipo_banco = "mysql";
-        $this->servidor = "localhost";
-        $this->usuario = "root";
-        $this->senha = "";
+        $this->dataBase_type = "mysql";
+        $this->server = "localhost";
+        $this->user = "root";
+        $this->password = "";
         $this->db = "radar_criminal_teste";
-        $this->banco = NewADOConnection($this->tipo_banco);
-        $this->banco->dialect = 3;
-        $this->banco->debug = false;
-        $this->banco->Connect($this->servidor, $this->usuario, $this->senha, $this->db);
+        $this->dataBase = NewADOConnection($this->dataBase_type);
+        $this->dataBase->dialect = 3;
+        $this->dataBase->debug = false;
+        $this->dataBase->Connect($this->server, $this->user, $this->password, $this->db);
     }
 
 }
