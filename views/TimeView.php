300616c72aca9a2e802bc03b1f002a0768d3a61a
Tainara Reis <tainara.s.r@hotmail.com>
2014-05-23 20:33:27 -0300

    Including PhpDoc in TimeView.php

--- views/TimeView.php
+++ views/TimeView.php
@@ -4,28 +4,35 @@
   File name: TimeView.php
   File description: return time
  */
+
+/**Class for objects of type TimeView, which will be contained, values ​​and methods for the same. 
+
+  * @since 2013
+
+ */
 include_once('C:/xampp/htdocs/mds2013/controller/TimeController.php');
 
 class TimeView {
 
     private $timeCO;
 
+    /** Method to instantiate a TimeController
+      * @return void
+    */
     public function __construct() {
         $this->tempoCO = new TimeController();
     }
 
+    /** Method to return a data formated
+      * @return Array of integers - Data
+    */
     public function returnDataTimeFormated() {
         return $this->tempoCO->_returnDataFormated();
     }
 
-    //Metodo duplicado para adaptacao do retorno de dados para a nova interface
-    /**
-     * @author Eduardo Augusto
-     * @author Sergio Silva
-     * @author Eliseu Egewarth
-     * @copyright RadarCriminal 2013
-     * */
-    
+    /** Method to return a data formated
+      * @return Array of integers - Data
+    */
     public function returnDataTimeFormatedNew() {
         return $this->tempoCO->_returnDataFormatNew();
     }
